0ef1ec21f75567d1d8ba9c9e70160d220a44bae3
GeovanneSousa <56236554+Neutro17@users.noreply.github.com>
2020-06-17 23:19:43 -0300

    Create Cliente.cs

---
+++ Aula01_17_Jun_2020/Cliente.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA_Banco2.Model
+{
+    public class Cliente
+    {
+        //atributos
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+
+        //metodos
+        public void MudaEndereco() { }
+        public void FazEmprestimo() { }
+    }
+}
