57e244b6e23ffc8a87e0ddb506f14dd52a5bd515
GeovanneSousa <56236554+Neutro17@users.noreply.github.com>
2020-06-17 23:23:46 -0300

    Add files via upload

---
+++ Aula02__18_Jun_2020/Model/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA_Escola.Model
+{
+    public class Aluno
+    {
+        //atributos
+        public string nome { get; set; }
+        public string idade { get; set; }
+        public string email { get; set; }
+    }
+}
