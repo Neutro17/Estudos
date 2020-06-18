d810566a7401815e7c4990bbf726d04d699c6820
GeovanneSousa <56236554+Neutro17@users.noreply.github.com>
2020-06-17 23:20:25 -0300

    Add files via upload

---
+++ Aula01_17_Jun_2020/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CA_Banco2.Model
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+        public double Salario { get; set; }
+    }
+}
---
+++ Aula01_17_Jun_2020/Program.cs
@@ -0,0 +1,25 @@
+﻿using CA_Banco2.Model;
+using System;
+
+
+namespace CA_Banco2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cli1 = new Cliente();
+            cli1.Codigo = "1";
+            cli1.Nome = "José";
+
+            Funcionario func1 = new Funcionario();
+            func1.Codigo = "1";
+            func1.Nome = "Maria";
+            func1.Salario = 1000;
+
+            Console.WriteLine("Nome do Cliente: = {0}", cli1.Nome);
+            Console.WriteLine("Nome do funcionário(a): = {0}", func1.Nome);
+            Console.ReadKey();
+        }
+    }
+}
